63bb34f42b0742384296aada7578cee9eb2d8822
Adrian Anaemeje <anaemeje@mit.edu>
2023-10-03 22:01:03 -0400

    Add Message and Status concept

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,7 @@
 import FriendConcept from "./concepts/friend";
+import MessageConcept from "./concepts/message";
 import PostConcept from "./concepts/post";
+import StatusConcept from "./concepts/status";
 import UserConcept from "./concepts/user";
 import WebSessionConcept from "./concepts/websession";
 
@@ -8,3 +10,5 @@ export const WebSession = new WebSessionConcept();
 export const User = new UserConcept();
 export const Post = new PostConcept();
 export const Friend = new FriendConcept();
+export const Status = new StatusConcept();
+export const Message = new MessageConcept();
---
+++ server/concepts/status.ts
@@ -0,0 +1,66 @@
+import { Filter, ObjectId } from "mongodb";
+import DocCollection, { BaseDoc } from "../framework/doc";
+import { NotAllowedError } from "./errors";
+
+export interface StatusDoc extends BaseDoc {
+  user: ObjectId;
+  content: string;
+}
+
+export default class StatusConcept {
+  public readonly statuses = new DocCollection<StatusDoc>("statuses");
+
+  async create(user: ObjectId, content: string) {
+    // TODO: Delete if already exists
+    const _id = await this.statuses.createOne({ user, content });
+    return { msg: "Status created!", status: await this.statuses.readOne({ _id }) };
+  }
+
+  async getStatus(query: Filter<StatusDoc>) {
+    return await this.statuses.readMany(query);
+  }
+
+  async getUserStatus(user: ObjectId) {
+    return await this.statuses.readOne({ user });
+  }
+
+  async update(_id: ObjectId, update: Partial<StatusDoc>) {
+    this.sanitizeUpdate(update);
+    await this.statuses.updateOne({ _id }, update);
+    return { msg: "Status successfully updated!" };
+  }
+
+  async delete(_id: ObjectId) {
+    await this.statuses.deleteOne({ _id });
+    return { msg: "Status deleted successfully!" };
+  }
+
+  async isUser(user: ObjectId, _id: ObjectId) {
+    const status = await this.statuses.readOne({ _id });
+    if (!status) {
+      throw new NotAllowedError(`Status ${_id} does not exist!`);
+    }
+
+    if (status.user.toString() !== user.toString()) {
+      throw new StatusUserNotMatchError(user, _id);
+    }
+  }
+
+  private sanitizeUpdate(update: Partial<StatusDoc>) {
+    // Make sure the update cannot change the user.
+    for (const key in update) {
+      if (key !== "content") {
+        throw new NotAllowedError(`Cannot update '${key}' field!`);
+      }
+    }
+  }
+}
+
+export class StatusUserNotMatchError extends NotAllowedError {
+  constructor(
+    public readonly user: ObjectId,
+    public readonly _id: ObjectId,
+  ) {
+    super("{0} is not the user associated with status {1}!", user, _id);
+  }
+}
